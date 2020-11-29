185551f75b526bad0b1150e2728635c4ee5d6fb1
greglongo <gregorylongo@gmail.com>
2020-11-29 01:00:43 -0800

    social , menus

--- src/js/burger-menu-script.js
+++ src/js/burger-menu-script.js
@@ -2,7 +2,7 @@ jQuery(document).ready(function() {
     console.log('ifired');
      var $hamburger = $(".hamburger");
 	  $hamburger.on("click", function(e) {
-	    $hamburger.toggleClass("is-active");
+	    $hamburger.toggleClass("push");
 	    // Do something else, like open/close menu
 	    $(".nav").toggleClass("is-active");
 	   	$(".page-template").toggleClass("push");
